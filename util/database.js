4771e058b4d60a055789ce37f44859aea93b4fa1
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-02-22 13:20:19 -0600

    create mysql pool connection

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node-complete',
+  password: 'process.env.PASS'
+})
+
+module.exports = pool.promise();
+
